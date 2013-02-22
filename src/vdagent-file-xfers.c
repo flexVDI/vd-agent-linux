029843152cbb72c5fc96eef332fbcb240c436b58
Hans de Goede <hdegoede@redhat.com>
2013-02-22 16:08:29 +0100

    file-xfers: Prefix all log messages with "file-xfer"
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- src/vdagent-file-xfers.c
+++ src/vdagent-file-xfers.c
@@ -25,6 +25,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
 #include <string.h>
 #include <syslog.h>
 #include <unistd.h>
@@ -95,7 +96,7 @@ static AgentFileXferTask *vdagent_parse_start_msg(
                                   (const gchar *)msg->data,
                                   -1,
                                   G_KEY_FILE_NONE, &error) == FALSE) {
-        syslog(LOG_ERR, "failed to load keyfile from data, error:%s\n",
+        syslog(LOG_ERR, "file-xfer: failed to load keyfile: %s",
                error->message);
         goto error;
     }
@@ -104,13 +105,15 @@ static AgentFileXferTask *vdagent_parse_start_msg(
     task->file_name = g_key_file_get_string(
         keyfile, "vdagent-file-xfer", "name", &error);
     if (error) {
-        syslog(LOG_ERR, "failed to parse filename, error:%s\n", error->message);
+        syslog(LOG_ERR, "file-xfer: failed to parse filename: %s",
+               error->message);
         goto error;
     }
     task->file_size = g_key_file_get_uint64(
         keyfile, "vdagent-file-xfer", "size", &error);
     if (error) {
-        syslog(LOG_ERR, "failed to parse filesize, error:%s\n", error->message);
+        syslog(LOG_ERR, "file-xfer: failed to parse filesize: %s",
+               error->message);
         goto error;
     }
 
@@ -130,7 +133,7 @@ void vdagent_file_xfers_start(struct vdagent_file_xfers *xfers,
     VDAgentFileXferStartMessage *msg)
 {
     AgentFileXferTask *new;
-    char *file_path;
+    char *file_path = NULL;
     const gchar *desktop;
 
     new = vdagent_parse_start_msg(msg);
@@ -144,13 +147,15 @@ void vdagent_file_xfers_start(struct vdagent_file_xfers *xfers,
     }
     file_path = g_build_filename(desktop, new->file_name, NULL);
     new->file_fd = open(file_path, O_CREAT | O_WRONLY, 0644);
-    g_free(file_path);
     if (new->file_fd == -1) {
-        syslog(LOG_ERR, "Create file error:%s\n", strerror(errno));
+        syslog(LOG_ERR, "file-xfer: failed to create file %s: %s",
+               file_path, strerror(errno));
         goto error;
     }
 
     if (ftruncate(new->file_fd, new->file_size) < 0) {
+        syslog(LOG_ERR, "file-xfer: err reserving %"PRIu64" bytes for %s: %s",
+               new->file_size, file_path, strerror(errno));
         goto error;
     }
 
@@ -158,18 +163,21 @@ void vdagent_file_xfers_start(struct vdagent_file_xfers *xfers,
 
     udscs_write(xfers->vdagentd, VDAGENTD_FILE_XFER_STATUS,
                 msg->id, VD_AGENT_FILE_XFER_STATUS_CAN_SEND_DATA, NULL, 0);
+    g_free(file_path);
     return ;
 
 error:
     udscs_write(xfers->vdagentd, VDAGENTD_FILE_XFER_STATUS,
                 msg->id, VD_AGENT_FILE_XFER_STATUS_ERROR, NULL, 0);
     vdagent_file_xfer_task_free(new);
+    g_free(file_path);
 }
 
 void vdagent_file_xfers_status(struct vdagent_file_xfers *xfers,
     VDAgentFileXferStatusMessage *msg)
 {
-    syslog(LOG_INFO, "task %d received response %d", msg->id, msg->result);
+    syslog(LOG_INFO, "file-xfer: task %d received response %d",
+           msg->id, msg->result);
 
     if (msg->result == VD_AGENT_FILE_XFER_STATUS_CAN_SEND_DATA) {
         /* Do nothing */
@@ -178,9 +186,9 @@ void vdagent_file_xfers_status(struct vdagent_file_xfers *xfers,
         gboolean found;
         found = g_hash_table_remove(xfers->xfers, GINT_TO_POINTER(msg->id));
         if (found) {
-            syslog(LOG_DEBUG, "remove task %d", msg->id);
+            syslog(LOG_DEBUG, "file-xfer: remove task %d", msg->id);
         } else {
-            syslog(LOG_ERR, "can not find task %d", msg->id);
+            syslog(LOG_ERR, "file-xfer: can not find task %d", msg->id);
         }
     }
 }
@@ -193,13 +201,14 @@ void vdagent_file_xfers_data(struct vdagent_file_xfers *xfers,
 
     task = g_hash_table_lookup(xfers->xfers, GINT_TO_POINTER(msg->id));
     if (task == NULL) {
-        syslog(LOG_INFO, "Can not find task:%d", msg->id);
+        syslog(LOG_INFO, "file-xfer: can not find task %d", msg->id);
         return ;
     }
 
     len = write(task->file_fd, msg->data, msg->size);
     if (len == -1) {
-        syslog(LOG_ERR, "write file error:%s\n", strerror(errno));
+        syslog(LOG_ERR, "file-xfer: error writing %s: %s", task->file_name,
+               strerror(errno));
         /* TODO: close, cancel dnd */
         return ;
     }
@@ -208,14 +217,14 @@ void vdagent_file_xfers_data(struct vdagent_file_xfers *xfers,
     if (task->read_bytes >= task->file_size) {
         gboolean found;
         if (task->read_bytes > task->file_size) {
-            syslog(LOG_ERR, "error: received too much data");
+            syslog(LOG_ERR, "file-xfer: error received too much data");
         }
-        syslog(LOG_DEBUG, "task %d have been finished", task->id);
+        syslog(LOG_DEBUG, "file-xfer: task %d has completed", task->id);
         found = g_hash_table_remove(xfers->xfers, GINT_TO_POINTER(msg->id));
         if (found) {
-            syslog(LOG_DEBUG, "remove task %d", msg->id);
+            syslog(LOG_DEBUG, "file-xfer: remove task %d", msg->id);
         } else {
-            syslog(LOG_ERR, "can not find task %d", msg->id);
+            syslog(LOG_ERR, "file-xfer: can not find task %d", msg->id);
         }
     }
 }
